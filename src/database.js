ce03bb55df882bdcc421f37a7cfdc300050cfe04
Ducks <cansgamer@gmail.com>
2024-12-24 00:22:03 -0500

    a

--- src/database.js
+++ src/database.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const { database } = require('./keys')
 
 mongoose.connect(database.URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true
 })
   .then(db => console.log("BD On"))
   .catch(err => console.error(err))
--- src/keys.js
+++ src/keys.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = {
   database: {
     // URI: 'mongodb://localhost/testing'
-    URI: 'mongodb+srv://ducks:<ducksito>@cluster0.b9esx.mongodb.net/?testing=true&w=majority&appName=Cluster0'
+    URI: 'mongodb+srv://ducks:ducksito@cluster0.b9esx.mongodb.net/testing=true&w=majority&appName=Cluster0'
   }
 }

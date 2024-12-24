40815164eeae22922dbe5ed11033a99c4c10a776
Ducks <cansgamer@gmail.com>
2024-12-24 00:52:02 -0500

    aea

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   database: {
     // URI: 'mongodb://localhost/testing'
-    URI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b9esx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
-    // URI: process.env.MONGODB_URI
+    // URI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b9esx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+    URI: process.env.MONGODB_URI
   }
 }
